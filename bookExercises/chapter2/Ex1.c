1a9ef317d0607539d44b68d7fc06684058894a28
arlind <imeriarlind93@gmail.com>
2026-03-20 14:33:48 +0100

    .

---
+++ bookExercises/chapter2/Ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int middleWhitespace, sideWhitespace, matrixSize = 10 ;	
+
+
+
+	for ( int i = 0 ; i < maxSize ; i++)
+	{
+		for ( int j = 0 ; j < maxSize ; j ++)
+		{
+			
+		}
+	}
+	return 0;
+}
