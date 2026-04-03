dcf7ff7d187f5df53fa80432b9e2b0131a59cad3
arlind <imeriarlind93@gmail.com>
2026-04-03 21:39:17 +0200

    check if a number has any repeted digits

---
+++ bookExercises/chapter8/repdigit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "stdbool.h"
+#define SIZE 10
+
+int main(void){
+
+    bool checkRepetedNumbers [SIZE];
+    int number;
+
+    printf("write a number ");
+    scanf("%d", &number);
+
+    //7
+
+    while (number > 0){
+
+        int temp = number % 10;
+
+        if(checkRepetedNumbers[temp]){
+            printf("Repeated digit\n");
+            return 0;
+        }
+        else {
+            checkRepetedNumbers[temp] = true;
+        }
+
+        number /= 10;
+    }
+
+
+    printf("No Repeated digit\n");
+    return  0;
+}
