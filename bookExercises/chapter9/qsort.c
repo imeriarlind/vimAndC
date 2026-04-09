439767abeff4a37788a5494747ab9517e7c6235f
arlind <imeriarlind93@gmail.com>
2026-04-09 16:59:12 +0200

    added the chapter 9 and started work in the first sort

---
+++ bookExercises/chapter9/qsort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int main(void){
+
+    int array [SIZE];
+    printf("write %d numbers", SIZE);
+
+    for (int i = 0 ; i < SIZE; i++ ) {
+        scanf("%d", &array[i]);
+    }
+
+}
+
+void quick_sort(int arr [], int start , int end){
+
+}
